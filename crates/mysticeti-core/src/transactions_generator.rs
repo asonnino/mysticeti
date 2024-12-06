3534f0b3d9bd329df5592370be83c7235b9ae914
Alberto Sonnino <alberto@sonnino.com>
2024-12-06 15:00:00 +0100

    feat: increase concurrency level for tests

--- crates/mysticeti-core/src/transactions_generator.rs
+++ crates/mysticeti-core/src/transactions_generator.rs
@@ -154,12 +154,12 @@ impl TransactionGenerator {
     fn update_budget(&mut self, counter: u64) {
         let committee_size = self.committee.len() as u64;
         let certified_transactions_count = self.certified_transactions_count();
-        // the client submits the same transaction to all validators.
-        let unique_certificates = if certified_transactions_count % committee_size == 0 {
-            certified_transactions_count / committee_size
-        } else {
-            certified_transactions_count / committee_size + 1
-        };
+        // let unique_certificates = if certified_transactions_count % committee_size == 0 {
+        //     certified_transactions_count / committee_size
+        // } else {
+        //     certified_transactions_count / committee_size + 1
+        // };
+        let unique_certificates = certified_transactions_count;
         tracing::warn!(
             "updating budget: certified_transactions_count={certified_transactions_count}, unique_certificates={unique_certificates}, counter={counter}"
         );
